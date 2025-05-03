05be983e37e02a9632bdf69837972735225268a1
tawfiq2725 <tawfiqsiraj003003@gmail.com>
2025-05-03 12:03:18 +0530

    favicon

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -33,6 +33,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link href="https://fonts.cdnfonts.com/css/satoshi" rel="stylesheet" />
+        <link rel="shortcut icon" href="/cmwlogo.png" type="image/x-icon" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body
